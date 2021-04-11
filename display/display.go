9847aea9b70b6a4e07a707ab42236ec756898583
Orion Dollar <orion.dollar@gmail.com>
2021-04-11 10:17:53 -0700

    tui

--- display/display.go
+++ display/display.go
@@ -2,25 +2,32 @@ package display
 
 import (
 	"github.com/gdamore/tcell/v2"
+	"github.com/oriondollar/fastrods/hrm"
 )
 
-func MakeBox(s tcell.Screen) {
+func MakeBox(s tcell.Screen, rodState []*hrm.Rod) {
 	w, h := s.Size()
 
 	if w == 0 || h == 0 {
 		return
 	}
 
+	box_end_w := w
+	box_end_h := int(float64(h) * 0.95)
+
 	st := tcell.StyleDefault
-	gl := ' '
+	mainc := ' '
 	if s.Colors() > 256 {
 		rgb := tcell.NewRGBColor(111, 26, 8)
-		st := st.Background(rgb)
+		st = st.Background(rgb)
+	} else if s.Colors() > 1 {
+		rgb := tcell.NewRGBColor(111, 26, 8)
+		st = st.Background(rgb)
 	}
 
-	for row := 0; row < h; row++ {
-		for col := 0; col < w; col++ {
-			s.SetContent(col, row, gl, ' ', st)
+	for row := 0; row < box_end_h; row++ {
+		for col := 0; col < box_end_w; col++ {
+			s.SetContent(col, row, mainc, nil, st)
 		}
 	}
 	s.Show()
